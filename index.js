a5fb404c7e5a46ab73fb887e1f5bdb36a2084e97
tanvirmahfuz100 <tanvirmahfuz100@gmail.com>
2025-04-08 01:35:08 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is working! 🚀');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
